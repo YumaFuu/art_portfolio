8168c190f8da5453ed44782b334d473f5fc80a5a
Yuma Ishikawa <yuma.fuu05@gmail.com>
2019-11-12 00:36:35 +0900

    [create]  seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  name: "ayano",
+  email: "fuyano07@gmail.com",
+  password: "ayanosite",
+  password_confirmation: "ayanosite"
+)
